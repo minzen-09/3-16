3d5e2c060dd2df5fab279f4d7fabaa3cbadef38a
EC2 Default User <ec2-user@ip-172-31-17-69.ap-northeast-1.compute.internal>
2024-11-02 10:42:49 +0000

    [Finish] 3-16

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,6 +74,6 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.hosts << "add828d4345243c5b5adf917d99ed183.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  config.hosts.clear
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_10_14_054327) do
+ActiveRecord::Schema.define(version: 2024_10_20_091214) do
 
-  create_table "reports", force: :cascade do |t|
+  create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", precision: 6, null: false
